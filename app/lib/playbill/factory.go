fb3410d98db6f5d1e54d5d881f984fc95fc68378
mzq <mazuqiang@lane.com>
2023-01-18 16:42:06 +0800

    init

--- app/lib/playbill/factory.go
+++ app/lib/playbill/factory.go
@@ -3,6 +3,7 @@ package playbill
 import (
 	vips "github.com/davidbyttow/govips/v2/vips"
 	"palybill/app/model"
+	"strings"
 )
 
 func Load(data model.Poster, changeData map[string]interface{}) {
@@ -21,30 +22,14 @@ func createArea(data model.Poster) (*vips.ImageRef, error) {
 	}
 	width = width / zoom
 	height = height / zoom
+
 	render := "<svg viewBox=\"0 0 {width} {height}\">\n" +
 		"<rect x=\"0\" y=\"0\"\n\theight=\"{height}\"\n\twidth=\"{width}\"\n\tfill-opacity=\"0\"\n>" +
 		"</rect>\n" +
 		"</svg>"
-	return vips.ImageTypeSVG
 
-	//	if (!$rows->data->opacity) {
-	//		return Image::black($rows->data->width / $rows->data->zoom, $rows->data->height / $rows->data->zoom);
-	//	}
-	//
-	//	$width = $rows->data->width / $rows->data->zoom;
-	//	$height = $rows->data->height / $rows->data->zoom;
-	//	$render = <<<EOF
-	//	<svg viewBox="0 0 {width} {height}">
-	//	<rect x="0" y="0"
-	//	height="{height}"
-	//	width="{width}"
-	//	fill-opacity="0"
-	//	></rect>
-	//	</svg>
-	//		EOF;
-	//	$render = strtr($render, [
-	//	'{width}' => $width,
-	//		'{height}' => $height,
-	//]);
-	//	return Image::svgload_buffer($render
+	render = strings.ReplaceAll(render, "{width}", string(width))
+	render = strings.ReplaceAll(render, "{height}", string(height))
+
+	return vips.LoadImageFromBuffer([]byte(render), &vips.ImportParams{})
 }
